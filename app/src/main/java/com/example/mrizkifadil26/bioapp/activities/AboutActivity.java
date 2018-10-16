ce2c9e0bf8582c8f291519888ada714f0ebb696d
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2018-10-16 17:49:32 +0700

    Completing About Page

--- app/src/main/java/com/example/mrizkifadil26/bioapp/activities/AboutActivity.java
+++ app/src/main/java/com/example/mrizkifadil26/bioapp/activities/AboutActivity.java
@@ -3,10 +3,15 @@ package com.example.mrizkifadil26.bioapp.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Gravity;
+import android.widget.Toast;
 
 import com.example.mrizkifadil26.bioapp.R;
 
+import java.util.Calendar;
+
 import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -18,12 +23,31 @@ public class AboutActivity extends AppCompatActivity {
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
                 .setImage(R.drawable.ic_biologo_60)
+                .addItem(new Element().setTitle("Version 1.0"))
                 .setDescription("Aplikasi ini merupakan aplikasi pengenalan biodiversitas tanaman di Suku Baduy.")
                 .addGroup("Connect with Us")
                 .addEmail("mrizkifadil26@gmail.com")
                 .addGitHub("mrizkifadil26")
+                .addItem(getCopyRightsElement())
                 .create();
 
         setContentView(aboutPage);
+    }
+
+    Element getCopyRightsElement() {
+        Element copyRightsElement = new Element();
+        final String copyrights = String.format(getString(R.string.copy_right), Calendar.getInstance().get(Calendar.YEAR));
+        copyRightsElement.setTitle(copyrights);
+        copyRightsElement.setIconDrawable(R.drawable.about_icon_copy_right);
+        copyRightsElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_item_icon_color);
+        copyRightsElement.setIconNightTint(android.R.color.white);
+        copyRightsElement.setGravity(Gravity.CENTER);
+        copyRightsElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AboutActivity.this, copyrights, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return copyRightsElement;
     }
 }
